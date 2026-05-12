d3b4505f81188a963d2de008fd89701880584237
Rojina <rozhina.agh@gmail.com>
2026-05-12 21:56:35 +0330

    Create circle_area.cpp

---
+++ circle_area.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+  float radius;
+
+  cout << "Enter the radius: ";
+  cin >> radius;
+
+  cout << "Area of the circle : ";
+
+  return 0;
+}
