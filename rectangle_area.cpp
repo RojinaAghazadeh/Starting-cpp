62f59fdac39523dd4c41a953cef2c34122216ff2
Rojina <rozhina.agh@gmail.com>
2026-05-12 21:54:14 +0330

    Create rectangle_area.cpp

---
+++ rectangle_area.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  float length, width;
+
+  cout << "Enter your rectangle's length: ";
+  cin >> length;
+
+  cout << "Enter your rectangle's width: ";
+  cin >> width;
+
+  cout << "Area of rectangle = " << endl;
+
+  return 0;
+}
